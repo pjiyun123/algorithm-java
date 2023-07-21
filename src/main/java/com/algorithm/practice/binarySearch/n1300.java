dce054de900891f64223ef8075e3595ddad8e990
parkjiyun <pjyun05200@gmail.com>
2023-07-21 11:22:48 +0900

    binarySearch-n1300

---
+++ src/main/java/com/algorithm/practice/binarySearch/n1300.java
@@ -0,0 +1,37 @@
+package com.algorithm.practice.binarySearch;
+
+import java.io.IOException;
+import java.util.*;
+
+public class n1300 {
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 배열의 크기
+        int K = sc.nextInt(); // 찾는 수의 순서
+
+        long lo = 1;
+        long hi = K;
+
+        while(lo < hi) {
+            long mid = (lo + hi) / 2;
+            long count = 0;
+
+            for(int i = 1; i <= N; i++) {
+                count += Math.min(mid/i, N);
+            }
+
+            if(K <= count) {
+                hi = mid;
+            }
+            else {
+                lo = mid + 1;
+            }
+        }
+
+        System.out.println(lo);
+
+    }
+
+}
