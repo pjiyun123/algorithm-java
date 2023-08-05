b12f65f120f5230cec3ae52ba3667a498de85c02
parkjiyun <pjyun05200@gmail.com>
2023-08-05 19:07:48 +0900

    binarySearch-n2473

---
+++ src/main/java/com/algorithm/practice/binarySearch/n2473.java
@@ -0,0 +1,56 @@
+package com.algorithm.practice.binarySearch;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class n2473 {
+
+    static long[] pick = new long[3];
+    static long max = 3000000000L;
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+
+        long[] arr = new long[N];
+        for(int i = 0; i < N; i++)
+            arr[i] = sc.nextLong();
+        Arrays.sort(arr);
+
+        for(int i = 0; i < N-2; i++)
+            solution(arr, i);
+
+        Arrays.sort(pick);
+        System.out.println(pick[0] + " " + pick[1] + " " + pick[2]);
+
+    }
+
+    public static void solution(long[] arr, int index) {
+        int left = index+1;
+        int right = arr.length-1;
+
+        while(left < right) {
+            long sum = arr[left] + arr[right] + arr[index];
+            long absSum = Math.abs(sum);
+
+            if(absSum < max) {
+                pick[0] = arr[left];
+                pick[1] = arr[right];
+                pick[2] = arr[index];
+                max = absSum;
+            }
+
+            if(sum > 0)
+                right--;
+            else
+                left++;
+        }
+    }
+
+
+
+}
