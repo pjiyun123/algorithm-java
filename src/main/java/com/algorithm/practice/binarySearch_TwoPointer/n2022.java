22c91ce2375426122a4e0f263f5af878289de1e5
parkjiyun <pjyun05200@gmail.com>
2023-10-06 18:23:56 +0900

    binarySearch-n2022

---
+++ src/main/java/com/algorithm/practice/binarySearch_TwoPointer/n2022.java
@@ -0,0 +1,36 @@
+package com.algorithm.practice.binarySearch_TwoPointer;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 사다리
+public class n2022 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        double x = Double.parseDouble(st.nextToken());
+        double y = Double.parseDouble(st.nextToken());
+        double c = Double.parseDouble(st.nextToken());
+
+        double left = 0;
+        double right = Math.min(x, y);
+
+        while(right - left >= 0.001) {
+            double width = (left + right) / 2;
+            double h1 = Math.sqrt(Math.pow(x, 2) - Math.pow(width, 2));
+            double h2 = Math.sqrt(Math.pow(y, 2) - Math.pow(width, 2));
+            double res = (h1 * h2) / (h1 + h2);
+
+            if(res >= c) left = width;
+            else right = width;
+        }
+
+        System.out.println(String.format("%.3f", right));
+
+    }
+
+}
